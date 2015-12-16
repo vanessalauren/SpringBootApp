8416de701a144ea6863d4854f9dae467367c5f5e
Vanessa Anderson <vanessa.anderson@timeinc.com>
2015-12-16 12:13:44 -0500

    add eb init force parameters

--- setup-eb.sh
+++ setup-eb.sh
@@ -3,3 +3,6 @@ set -e
 
 mkdir ./elasticbeanstalk/
 mv config.yml ./elasticbeanstalk/config.yml
+cd ./elasticbeanstalk
+ls
+eb init -f -I "$AWS_ACCESS_KEY_ID" -S "$AWS_SECRET_ACCESS_KEY" -a "sloos" -e "timeinc-docker-dev" --region us-east-1 -s "64bit Amazon Linux running Docker 1.7"
