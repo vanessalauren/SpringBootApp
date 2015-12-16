e660595444078306a813e34cb4d4e397cc5502a3
Vanessa Anderson <vanessa.anderson@timeinc.com>
2015-12-16 12:33:51 -0500

    add docker caching to circleci, add eb cli init call

--- setup-eb.sh
+++ setup-eb.sh
@@ -1,8 +1,14 @@
 set -x
 set -e
 
+mkdir /home/ubuntu/.aws
+touch /home/ubuntu/.aws/config
+chmod 600 /home/ubuntu/.aws/config
+echo "[profile eb-cli]" > /home/ubuntu/.aws/config
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
 mkdir ./elasticbeanstalk/
 mv config.yml ./elasticbeanstalk/config.yml
 cd ./elasticbeanstalk
 ls
-eb init -f -I "$AWS_ACCESS_KEY_ID" -S "$AWS_SECRET_ACCESS_KEY" -a "sloos" -e "timeinc-docker-dev" --region us-east-1 -s "64bit Amazon Linux running Docker 1.7"
+eb init sloos --profile default -r us-east-1 -p Docker -k ti-use1-bdprod-docker -i -v
